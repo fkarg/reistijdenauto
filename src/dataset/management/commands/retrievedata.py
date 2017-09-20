1811c88d2f5513ba78229cb7fd718c5212aaa502
Thijs Coenen <info@thylacine.nl>
2017-09-20 18:16:14 +0200

    Initial commit for prototype travel time API.

---
+++ src/dataset/management/commands/retrievedata.py
@@ -0,0 +1,74 @@
+import tempfile
+import os
+
+import requests
+
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.gis.gdal import DataSource
+from django.contrib.gis.utils import LayerMapping
+
+from django.db import transaction
+
+
+from dataset.models import WegStuk
+
+
+TARGET = 'http://web.redant.net/~amsterdam/ndw/data/reistijdenAmsterdam.geojson'
+
+mapping = {
+    '_id': 'Id',
+    'name': 'Name',
+    '_type': 'Type',
+    'timestamp': 'Timestamp',
+    'length': 'Length',
+    'traveltime': 'Traveltime',
+    'velocity': 'Velocity',
+    'mline': 'LINESTRING'
+}
+
+
+class Command(BaseCommand):
+    help = 'Retrieve a new reistijden GeoJSON and store in the database.'
+
+    def handle(self, *args, **options):
+        self.stdout.write(self.style.SUCCESS('Downloaded GeoJSON'))
+
+        # TODO: research interactions of /tmp temporary directories and
+        # container file system LOOK FOR: --mount type=tempfs ...
+
+        self._download_geojson_and_store()
+
+        self.stdout.write(self.style.SUCCESS(
+            WegStuk.objects.count()
+        ))
+
+    def _download_geojson_and_store(self):
+        """
+        Download Reistijden GeoJSON and save it to the database.
+        """
+        with tempfile.TemporaryDirectory(dir='/tmp') as temp_dir:
+            temp_file = os.path.join(temp_dir, 'reistijdenAmsterdam.geojson')
+
+            r = requests.get(TARGET)
+            with open(temp_file, 'w') as f:
+                f.write(r.text)
+
+            ds = DataSource(temp_file)
+            self._sanity_check_datasource(ds)
+
+            self.stdout.write(self.style.SUCCESS(
+                'Datasource {} was opened'.format(ds.name)))
+
+            lm = LayerMapping(WegStuk, ds, mapping)
+
+            with transaction.atomic():
+                WegStuk.objects.all().delete()
+                lm.save(strict=True, verbose=False)
+
+    def _sanity_check_datasource(self, ds):
+        """
+        Hardcoded sanity checks, assumes inputs do not change.
+        """
+        if len(ds) != 1:
+            raise CommandError('GeoJSON should have only 1 layer.')
+        # TODO: add more checks
---
+++ src/dataset/models.py
@@ -0,0 +1,42 @@
+#  INFO: Open of `reistijdenAmsterdam.geojson'
+#        using driver `GeoJSON' successful.
+#
+#  Layer name: reistijdenAmsterdam
+#  Geometry: Line String
+#  Feature Count: 1012
+#  Extent: (4.704348, 52.246392) - (5.177359, 52.475149)
+#  Layer SRS WKT:
+#  GEOGCS["WGS 84",
+#      DATUM["WGS_1984",
+#          SPHEROID["WGS 84",6378137,298.257223563,
+#              AUTHORITY["EPSG","7030"]],
+#          AUTHORITY["EPSG","6326"]],
+#      PRIMEM["Greenwich",0,
+#          AUTHORITY["EPSG","8901"]],
+#      UNIT["degree",0.0174532925199433,
+#          AUTHORITY["EPSG","9122"]],
+#      AUTHORITY["EPSG","4326"]]
+#  Id: String (0.0)
+#  Name: String (0.0)
+#  Type: String (0.0)
+#  Timestamp: DateTime (0.0)
+#  Length: Integer (0.0)
+#  Traveltime: Integer (0.0)
+#  Velocity: Integer (0.0)
+
+from django.contrib.gis.db import models
+
+
+class WegStuk(models.Model):
+    _id = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
+    _type = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(max_length=255)   # convert to datetime
+    length = models.IntegerField()
+    traveltime = models.IntegerField()
+    velocity = models.IntegerField()
+
+    mline = models.LineStringField()
+
+    def __str__(self):
+        return self.name
